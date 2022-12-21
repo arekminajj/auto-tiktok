40e5db13148ad0aed88ab5915bde45344c799190
arekminajj <arek123113@gmail.com>
2022-12-21 18:47:06 +0100

    add text-content.py

---
+++ text-content.py
@@ -0,0 +1,35 @@
+import praw
+import random
+from decouple import config
+
+submissions = []
+
+reddit = praw.Reddit(
+    client_id=config('CLIENT_ID'),
+    client_secret=config('CLIENT_SECRET'),
+    password=config('PASSWORD'),
+    user_agent=config('USER_AGENT'),
+    username=config('USERNAME'),
+)
+
+subreddits = [
+    'AmItheAsshole',
+    'confession',
+    'nosleep'
+]
+
+def getRandomSubreddit():
+    randomSubreddit = random.choice(subreddits)
+
+    return randomSubreddit
+
+def getRandomSubmission():
+    subreddit = getRandomSubreddit()
+    for submission in reddit.subreddit(subreddit).hot(limit=25):
+        submissions.append(submission.selftext)
+    
+    randomSubmission = random.choice(submissions)
+
+    return randomSubmission
+
+print(getRandomSubmission())
