72f11b6b429519d6419380e507aedd7b71cb49d8
arekminajj <arek123113@gmail.com>
2022-12-23 20:03:43 +0100

    Change textToSpeech from gtts to google cloud api

--- auto-tiktok/textToSpeech.py
+++ auto-tiktok/textToSpeech.py
@@ -1,8 +1,23 @@
-from gtts import gTTS
+from google.cloud import texttospeech
+
 
 def textToSpeech(text):
-    language = 'en'
-    voice = gTTS(text=text, lang=language, slow=False)
-  
-    voice.save("././files/voice.mp3")
-    
+    client = texttospeech.TextToSpeechClient.from_service_account_file(
+        './googlecloudkey.json')
+
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    with open("././files/voice.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "voice.mp3"')
