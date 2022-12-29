9cc583d9e3ba966a49508905c41226ee4d9e2f62
arekminajj <arek123113@gmail.com>
2022-12-29 23:44:52 +0100

    make subtitles render in the center

--- auto-tiktok/video.py
+++ auto-tiktok/video.py
@@ -9,7 +9,7 @@ def addVoiceoverAndSubtitlesToVideo():
 
     input_video = ffmpeg.input('././files/RAW_VIDEO.mp4')
     input_audio = ffmpeg.input('././files/voice.mp3')
-    ffmpeg.concat(input_video.filter("subtitles", '././files/subtitles.srt'),
+    ffmpeg.concat(input_video.filter("subtitles", '././files/subtitles.srt', force_style="Alignment=10"),
                   input_audio, v=1, a=1).output('././files/video_final.mp4').run()
 
     print('video has been saved in: ././files/video_final.mp4')
